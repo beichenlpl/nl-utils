d9382e883d46326926f5f689746423c0d3dc4300
beichenlpl <81348554+beichenlpl@users.noreply.github.com>
2024-01-30 13:53:29 +0800

    Delete nl-utils-net directory

--- nl-utils-net/src/main/java/com/github/beichenlpl/net/http/HttpClient.java
+++
@@ -1,9 +0,0 @@
-package com.github.beichenlpl.net.http;
-
-/**
- * @author lpl
- * @version 1.0
- * @since 2024.01.25
- */
-public class HttpClient {
-}
--- nl-utils-net/src/main/java/com/github/beichenlpl/net/http/enums/HttpResponseCode.java
+++
@@ -1,9 +0,0 @@
-package com.github.beichenlpl.net.http.enums;
-
-/**
- * @author lpl
- * @version 1.0
- * @since 2024.01.25
- */
-public enum HttpResponseCode {
-}
